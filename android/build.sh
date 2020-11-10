9b62aedfef4810c44e475bf3ad4b215f7ae75129
Gabriele <gabboxl0@gmail.com>
2020-11-10 22:07:29 +0100

    adapted script for my builds

--- android/build.sh
+++ android/build.sh
@@ -2,11 +2,11 @@
 echo "--- Setup"
 export USE_CCACHE="1"
 export CCACHE_EXEC=/usr/bin/ccache
-export PYTHONDONTWRITEBYTECODE=true
-export BUILD_ENFORCE_SELINUX=1
+#export PYTHONDONTWRITEBYTECODE=true
+#export BUILD_ENFORCE_SELINUX=1
 export BUILD_NO=
 unset BUILD_NUMBER
-export OVERRIDE_TARGET_FLATTEN_APEX=true 
+#export OVERRIDE_TARGET_FLATTEN_APEX=true 
 #TODO(zif): convert this to a runtime check, grep "sse4_2.*popcnt" /proc/cpuinfo
 export CPU_SSE42=false
 # Following env is set from build
@@ -30,10 +30,14 @@ echo "--- Syncing"
 
 cd /lineage/${VERSION}
 rm -rf .repo/local_manifests/*
+
 if [ -f /lineage/setup.sh ]; then
     source /lineage/setup.sh
 fi
 yes | repo init -u https://github.com/lineageos/android.git -b ${VERSION}
+
+wget https://raw.githubusercontent.com/Gabboxl/local_manifests/master/msm8916_q.xml -P .repo/local_manifests/
+
 echo "Resetting build tree"
 repo forall -vc "git reset --hard" > /tmp/android-reset.log 2>&1
 echo "Syncing"
@@ -44,26 +48,10 @@ repo sync -j32 -d --force-sync > /tmp/android-sync.log 2>&1
 echo "--- clobber"
 rm -rf out
 
-echo "--- breakfast"
-set +e
-breakfast lineage_${DEVICE}-${TYPE}
-set -e
-
-if [[ "$TARGET_PRODUCT" != lineage_* ]]; then
-    echo "Breakfast failed, exiting"
-    exit 1
-fi
-
-if [ "$RELEASE_TYPE" '==' "experimental" ]; then
-  if [ -n "$EXP_PICK_CHANGES" ]; then
-    repopick $EXP_PICK_CHANGES
-  fi
-fi
 echo "--- Building"
-export OVERRIDE_TARGET_FLATTEN_APEX=true 
-mka otatools-package target-files-package dist > /tmp/android-build.log
+brunch lineage_gprimeltexx-userdebug > /tmp/android-build.log
 
-echo "--- Uploading"
-ssh jenkins@blob.lineageos.org mkdir -p /home/jenkins/incoming/${DEVICE}/${BUILD_UUID}/
-scp out/dist/*target_files*.zip jenkins@blob.lineageos.org:/home/jenkins/incoming/${DEVICE}/${BUILD_UUID}/
-scp out/target/product/${DEVICE}/otatools.zip jenkins@blob.lineageos.org:/home/jenkins/incoming/${DEVICE}/${BUILD_UUID}/
+#echo "--- Uploading"
+#ssh jenkins@blob.lineageos.org mkdir -p /home/jenkins/incoming/${DEVICE}/${BUILD_UUID}/
+#scp out/dist/*target_files*.zip jenkins@blob.lineageos.org:/home/jenkins/incoming/${DEVICE}/${BUILD_UUID}/
+#scp out/target/product/${DEVICE}/otatools.zip jenkins@blob.lineageos.org:/home/jenkins/incoming/${DEVICE}/${BUILD_UUID}/
